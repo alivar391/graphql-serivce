eb17422cfead7ebf911f40aa2cba0445eb4f36e6
alivar391 <patriot391@mail.ru>
2022-07-09 00:26:35 +0300

    refactor: refactor tracks module

--- src/modules/tracks/resolvers/tracks.resolver.ts
+++ src/modules/tracks/resolvers/tracks.resolver.ts
@@ -23,41 +23,41 @@ export class TracksResolver {
     private readonly artistsService: ArtistsService,
     private readonly albumsService: AlbumsService,
   ) {}
-  @Query('getTracks')
-  getTracks(@Args('limit') limit: number, @Args('offset') offset: number) {
+  @Query('tracks')
+  tracks(@Args('limit') limit: number, @Args('offset') offset: number) {
     return this.tracksService.getTracks(limit, offset);
   }
 
-  @Query('getTrack')
-  getTrack(@Args('id') id: string) {
+  @Query('track')
+  track(@Args('id') id: string) {
     return this.tracksService.getTrack(id);
   }
 
   @ResolveField()
-  album(@Parent() getTrack) {
-    const { albumId } = getTrack;
+  album(@Parent() track) {
+    const { albumId } = track;
     return this.albumsService.getAlbum(albumId);
   }
 
   @ResolveField()
-  genres(@Parent() getTrack) {
-    const { genresIds } = getTrack;
+  genres(@Parent() track) {
+    const { genresIds } = track;
     return genresIds.map(async (genreId) => {
       return this.genresService.getGenre(genreId);
     });
   }
 
   @ResolveField()
-  bands(@Parent() getTrack) {
-    const { bandsIds } = getTrack;
+  bands(@Parent() track) {
+    const { bandsIds } = track;
     return bandsIds.map(async (bandId) => {
       return this.bandsService.getBand(bandId);
     });
   }
 
   @ResolveField()
-  artists(@Parent() getTrack) {
-    const { artistsIds } = getTrack;
+  artists(@Parent() track) {
+    const { artistsIds } = track;
     return artistsIds.map(async (artistId) => {
       return this.artistsService.getArtist(artistId);
     });
